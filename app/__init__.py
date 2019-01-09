a7c2273dc651779b7ee596f2152fdf374ecb9ab9
Simon Siva <simonsiva13@gmail.com>
2019-01-10 01:13:31 +0300

    [finishes #163050265] set up dir structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from instance.config import app_config
-from .api.v1.views.meetups_views import meetups_bp
-from .api.v1.views.question_views import question_bp
+#from .api.v1.views.meetups_views import meetups_bp
+#from .api.v1.views.question_views import question_bp#
 
 def create_app(config_name):
 
