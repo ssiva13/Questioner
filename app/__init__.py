8651103ac72fbfe455d9b2c0aaede7c48a9d408e
Simon Siva <simonsiva13@gmail.com>
2019-01-10 05:47:57 +0300

    [finishes #163046194] finished post question feature

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+
+from flask import Flask
+from instance.config import app_config
+from .api.v1.views.meetups_views import meetups_bp
+from .api.v1.views.questions_views import question_bp
+
+def create_app(config_name):
+
+    """ Using the config file in instance folder to create app """
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object(app_config)
+    
+    app.register_blueprint(meetups_bp)
+    app.register_blueprint(question_bp)
+    
+    
+    return app
