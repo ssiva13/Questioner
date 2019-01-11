deb455fc7600edae6cda40920c86851f263e255b
Simon Siva <simonsiva13@gmail.com>
2019-01-11 12:17:18 +0300

    [start #163140805] --started cleaning import bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from instance.config import app_config
-#from .api.v1.views.meetups_views import meetups_bp
-#from .api.v1.views.question_views import question_bp#
+from .api.v1.views.meetups_views import meetups_bp
+from .api.v1.views.question_views import question_bp
+
 
 def create_app(config_name):
 
@@ -11,8 +12,10 @@ def create_app(config_name):
 
     app.config.from_object(app_config)
     
-    ##app.register_blueprint(meetups_bp)
-    ##app.register_blueprint(question_bp)
+
+    app.register_blueprint(meetups_bp)
+    app.register_blueprint(question_bp)
+    
     
     
     return app
