f9b44601d1218544a4d555a7acd8e7562ef80a67
Simon Siva <simonsiva13@gmail.com>
2019-01-14 02:37:32 +0300

    [start #163046192] Added upvote/downvote endpoints

--- app/api/v1/models/questions_models.py
+++ app/api/v1/models/questions_models.py
@@ -4,14 +4,32 @@
 import datetime
 
 """ Initialize list to hold all questions"""
-Questions = []
+Questions = [
+	{
+			"body" : "How do I prepare a rice nursery",
+			"createdOn" : "",
+			"createdBy" : "Simon Siva",
+			"meetup" : 2,
+			"title" : "Nursery",
+			"q_id" : 1,
+			"votes" : 4
+	   		},{
+			"body" : "How do I prepare a rice nursery",
+			"createdOn" : "",
+			"createdBy" : "Simon Siva",
+			"meetup" : 2,
+			"title" : "Nursery",
+			"votes" : 9,
+			"q_id" : 2
+	   		}
+]
 
 
 """Class model for storing meetups data"""
 
 class QuestionModel():
 	"""Meetup instance variable"""
-	def __init__(self,createdOn,createdBy, body, title,meetup,votes):
+	def __init__(self,createdBy, createdOn, body, title,meetup,votes):
 
 		createdOn=datetime.datetime.now()
 		createdOn=createdOn.strftime("%d/%m/%Y")
@@ -37,7 +55,22 @@ class QuestionModel():
 		}
 		Questions.append(question)
 		return Questions
-  
+
+	@classmethod
+	def vote_question(cls, q_id):
+		"""check if question id is greater than length of dict"""
+
+		if q_id<=len(Questions):	
+			que = Questions[q_id-1]
+			que1 = {
+				"q_id" 	: 	que["q_id"],
+				"title"	:	que["title"],
+				"meetup"	:	que["meetup"],
+				"body"	:	que["body"],
+				"votes"	:	que["votes"]
+				}
+			return que1
+		return "The question record was not found"
   
 	
 	
